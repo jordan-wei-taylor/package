cea5d14da8fb2775e79c1322c4a04e3aaff7926c
Jordan Taylor <jt2006@bath.ac.uk>
2022-06-23 09:51:04 +0100

    template

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# package
+# package template
+
+Change variables in the ``setup.py`` file as appropriate.
+
+It is always recommended to have a virtual python environment.
+
+```console
+# create virtual python environment if you do not have one
+python3 -m venv env
+
+# activate it
+# Mac OS / Linux
+source env/bin/activate
+
+# Windows
+./env/Scripts/activate
+```
+
+To install locally run
+
+```console
+pip3 install .
+```
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+def read(path):
+    with open(path, encoding = 'utf-8') as f:
+        return f.read()
+
+username = 'jordan-wei-taylor'
+name     = 'package'
+version  = '0.0.1'
+author   = 'Jordan Taylor'
+email    = 'jt2006@bath.ac.uk'
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=email,
+    description="A short description of your package",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{username}/{name}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{username}/{name}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    license=read('LICENSE'), 
+    install_requires=[
+        # 'numpy>=1.22.3', # example requirements
+        # 'psutil>=5.9.0',
+    ]
+)
